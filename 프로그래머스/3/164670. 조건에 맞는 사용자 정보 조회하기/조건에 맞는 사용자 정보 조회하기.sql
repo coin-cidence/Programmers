594a5f7e7dfefb6044d25e2c1943c3376addd907
coin-cidence <146698848+coin-cidence@users.noreply.github.com>
2024-09-08 14:46:51 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT U.USER_ID,
+       U.NICKNAME,
+       U.CITY ||' '|| U.STREET_ADDRESS1 || ' '|| U.STREET_ADDRESS2 AS 전체주소,
+       SUBSTR(TLNO, 1, 3) || '-' ||SUBSTR(TLNO, 4, 4) || '-' ||SUBSTR(TLNO, 8,4) AS 전화번호
+  FROM USED_GOODS_USER U 
+ WHERE U.USER_ID IN
+        (SELECT B.WRITER_ID
+           FROM USED_GOODS_BOARD B
+          GROUP BY B.WRITER_ID
+         HAVING COUNT(B.WRITER_ID)>=3)
+ ORDER BY U.USER_ID DESC;
